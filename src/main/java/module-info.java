f3226f72d6c5ea8eacda84a63cf1194aed066d76
hybras <24651269+hybras@users.noreply.github.com>
2020-02-26 23:03:30 -0500

    Add module info descriptor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.raiderrobotix.scouting.client {
+	requires lombok;
+	requires com.google.gson;
+	requires kotlin.stdlib;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.controls;
+}
