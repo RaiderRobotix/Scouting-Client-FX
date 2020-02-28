68fe96c109ddf9e3227d5683e9f5ce4ee319ffe4
hybras <24651269+hybras@users.noreply.github.com>
2020-02-27 21:53:49 -0500

    Add extra module-info.java deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
-module com.raiderrobotix.scouting.client {
+module org.raiderrobotix.scouting.client {
 	requires lombok;
 	requires com.google.gson;
 	requires kotlin.stdlib;
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.controls;
+	requires annotations;
+	requires blue.alliance.api.java.library;
+	requires commons.math3;
+	requires org.raiderrobotix.scouting.models;
 }
